6e4407ad9f6cee66c65ba847dda8d7982ab428bf
Carine TOdmia <ctodmia@gmail.com>
2015-11-23 15:26:13 -0800

    refractor: sign in views and routing perfected.

--- magcati/client/app/app.js
+++ magcati/client/app/app.js
@@ -3,31 +3,23 @@ angular.module('myApp', [
 ])
 .config( function($stateProvider, $urlRouterProvider, $locationProvider){
 
-	$urlRouterProvider.otherwise('/auth');
+	$urlRouterProvider.otherwise('/signup');
 
 	$stateProvider
-		.state('auth', { //this is the nav bar and at the end is a view for everything else
-			url: '/auth',
-			views: {
-				'': {templateUrl: 'app/auth/signin.html'},
-				'signUp@auth': {
-					templateUrl: 'app/auth/signup/signup.html'
-				}
-			}
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'app/auth/signup/signup.html'
 		})
-		// .state('auth.signup', {
-		// 	url: '/signup',
-		// 	templateUrl: 'app/auth/signup/signup.html'
-		// })
-		// .state('auth.mentorsignup', {
-		// 	url: '/mentor',
-		// 	templateUrl: 'app/auth/signup/mentorsignup.html'
-		// })
-		// .state('auth.menteesignup', {
-		// 	url: '/mentee',
-		// 	templateUrl: 'app/auth/signup/menteesignup.html'
-		// })
-		.state('auth.login', {
+		.state('mentee', {
+			url: '/mentee',
+			templateUrl: 'app/auth/signup/menteesignup.html'
+		})
+		.state('mentor', {
+			url: '/mentor',
+			templateUrl: 'app/auth/signup/mentorsignup.html'
+		})
+
+		.state('login', {
 			url: '/login',
 			templateUrl: 'app/auth/login/login.html'
 		})
