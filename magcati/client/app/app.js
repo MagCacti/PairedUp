5e91e59df54e9bca71dbe36c83948e0fa7f26a9c
Carine TOdmia <ctodmia@gmail.com>
2015-11-24 13:33:23 -0800

    feat: codeshare view added and implemented using ace. bower installed

--- magcati/client/app/app.js
+++ magcati/client/app/app.js
@@ -1,5 +1,6 @@
 angular.module('myApp', [
-	'ui.router'
+	'ui.router',
+	'ui.ace'
 ])
 .config( function($stateProvider, $urlRouterProvider, $locationProvider){
 
@@ -21,15 +22,40 @@ angular.module('myApp', [
 
 		.state('login', {
 			url: '/login',
-			templateUrl: 'app/auth/login/login.html'
+			templateUrl: 'app/auth/login/login.html',
+			controller: 'LoggedIn'
 		})
-		// .state('home', {
-		// 	url: '/home',
-		// 	templateUrl: 'app/home/home.html'
-		// })
+		.state('home', {
+			url: '/home',
+			templateUrl: 'app/home/home.html'
+		})
+
+		// codeshare was added using the following: https://github.com/angular-ui/ui-ace
+			
+		// 	you can also trying implementing the ace raw 
+
+		// 	https://ace.c9.io/
+
+		// 	in order to view this page you must install bower like so 
+		// 	bower install angular-ui-ace#bower
+		// 	or 
+		// 	bower install -g --save angular-ui-ace#bower
+
+		// 	whatever works for you
+		.state('codeshare', {
+			url: '/codeshare',
+			templateUrl: 'app/codeshare/codeshare.html'
+		})
+
+})
 
 
+.service('authToken', function() {
+    this.name='carine'
+    this.login = function(){console.log(this.name, "is logged in")}
+ 	
+})
 
-
-
+.controller('LoggedIn', function($scope, authToken) {
+	$scope.loggin = authToken.login();
 })
