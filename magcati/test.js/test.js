24ddb3b36ae6433a49c8aad1ab464de6947204d2
MachTech <aheri@Macbook-Pro.local>
2015-11-27 10:31:38 -0700

    typo: Corrected typo and added clarifying comments
    This makes the file and test implementation more useful to the team

--- magcati/test.js/test.js
+++ magcati/test.js/test.js
@@ -1,6 +1,10 @@
-var expect = require('chai').expect;
+// First, require Chai:
+var expect = require('chai').expect; 
 
-describe ('A bsic test', function () {
+// This is a simple true = true test.  
+// You can use it to ensure your testing environment is set up correctly (it will show 'passed'). 
+
+describe ('A basic test', function () {
   it('should pass when everything is okay', function () {
     expect(true).to.be.true;
   });
