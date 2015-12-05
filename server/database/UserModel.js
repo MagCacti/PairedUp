337ce667dbffd9debf827edfa0c24847e76925ee
MachTech <aheri@Macbook-Pro.local>
2015-12-05 10:39:36 -0800

    fix: Temporarily disconnected UserModel database for test deployment

--- server/database/UserModel.js
+++ server/database/UserModel.js
@@ -1,59 +1,59 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');å
+// var Schema = mongoose.Schema;
 
-//Line 5 or line 10. Not both. 
-/*mongodb:// username:password@ hostname:port/ database
-*/
+// //Line 5 or line 10. Not both. 
+// /*mongodb:// username:password@ hostname:port/ database
+// */
 
-// Since you're connecting to a local instance, you can skip the username and password and use the following URI:
-
-
-var uri = 'mongodb://localhost/users'; 
-var db = require('mongoose').connect(uri);
-
-var userSchema = new Schema({
- username: String,
- name: String,
- picture: String
- // lastname: String,
- // password: String, 
- // officeHours: String,
- // angular: String, 
- // node: String, 
- // javascript: String, 
- // skill: String,
- // interest: String
-});
+// // Since you're connecting to a local instance, you can skip the username and password and use the following URI:
 
 
-// userSchema.methods.speak = function () {
-//  var greeting = this.username? "Meow name is " + this.username: "I don't have a name";
-//  console.log(greeting);
-// }
+// var uri = 'mongodb://localhost/users'; 
+// var db = require('mongoose').connect(uri);
 
-
-
-var User = mongoose.model("User", userSchema);
-
-// var Joseph = new User({
-//  username: "JosephSun",
-//  firstname: "Bazz",
-//  lastname: "Khurshid",
-//  password: "YouKnowz", 
-//  officeHours: "9-10",
-//  angular: "65", 
-//  node: "70", 
-//  javascript: "80", 
-//  interest: "Databases, Augmented reality, Big Data"
+// var userSchema = new Schema({
+//  username: String,
+//  name: String,
+//  picture: String
+//  // lastname: String,
+//  // password: String, 
+//  // officeHours: String,
+//  // angular: String, 
+//  // node: String, 
+//  // javascript: String, 
+//  // skill: String,
+//  // interest: String
 // });
 
-// console.log("Joseph is speaking",Joseph.speak());
-// Joseph.save(function (err, Joseph) {
-//   if (err) return console.error(err);
-//   console.log("This is joseph", Joseph);
-//   Joseph.speak();
-// });
-/*Ultimate Product
 
-*/
-module.exports = User;
+// // userSchema.methods.speak = function () {
+// //  var greeting = this.username? "Meow name is " + this.username: "I don't have a name";
+// //  console.log(greeting);
+// // }
+
+
+
+// var User = mongoose.model("User", userSchema);
+
+// // var Joseph = new User({
+// //  username: "JosephSun",
+// //  firstname: "Bazz",
+// //  lastname: "Khurshid",
+// //  password: "YouKnowz", 
+// //  officeHours: "9-10",
+// //  angular: "65", 
+// //  node: "70", 
+// //  javascript: "80", 
+// //  interest: "Databases, Augmented reality, Big Data"
+// // });
+
+// // console.log("Joseph is speaking",Joseph.speak());
+// // Joseph.save(function (err, Joseph) {
+// //   if (err) return console.error(err);
+// //   console.log("This is joseph", Joseph);
+// //   Joseph.speak();
+// // });
+// /*Ultimate Product
+
+// */
+// module.exports = User;
