15ea59ca231b4de4b91f343d8e03f87339867920
MachTech <aheri@Macbook-Pro.local>
2015-12-09 20:43:09 -0800

    merge: Pulled and experimentally improved this file from Bkurshid's fork

---
+++ client/userprofile/userprofile.js
@@ -0,0 +1,13 @@
+var Joseph = {
+  username: "JosephSun",
+  firstname: "Bazz",
+  lastname: "Khurshid",
+  password: "YouKnowz", 
+  officeHours: "9-10",
+  angular: "65", 
+  node: "70", 
+  javascript: "80", 
+  interest: "Databases, Augmented reality, Big Data"
+};
+
+
