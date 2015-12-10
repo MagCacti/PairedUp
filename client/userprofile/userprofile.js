3b1038d73a92a3829113b7b49e762ddb4e15807d
MachTech <aheri@Macbook-Pro.local>
2015-12-10 15:28:48 -0800

    chore: Updated file structure to avoid routing conflict
    This will assist with deployment

--- client/userprofile/userprofile.js
+++ client/userprofile/userprofile.js
@@ -1,13 +1,1 @@
-var Joseph = {
-  username: "JosephSun",
-  firstname: "Bazz",
-  lastname: "Khurshid",
-  password: "YouKnowz", 
-  officeHours: "9-10",
-  angular: "65", 
-  node: "70", 
-  javascript: "80", 
-  interest: "Databases, Augmented reality, Big Data"
-};
-
-
+...userprofile.js here
