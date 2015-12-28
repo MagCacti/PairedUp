26728a3b28c0823ece46a53d7de5bd923a4fcdcd
BKhurshid <josephsun4@gmail.com>
2015-12-27 23:15:36 -0800

    Modularizing server code.

--- server/utils.js
+++ server/utils.js
@@ -1,13 +1,10 @@
-var dbUser = require('./userProfile/UserModel');
-var dbDocument = require('./documents/DocumentModel');
-var User = dbUser.user;
-var userDocument = dbDocument.userDocument;
 
 module.exports = {
   forceHTTPS: function(req, res, next) {
     var protocol = req.get('x-forwarded-proto');
     protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
   }, 
+
   allowCrossOrigin: function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
@@ -25,8 +22,8 @@ module.exports = {
         content = data;
         //To get around the synchronous behavior we wrap the next step into the function sendFileDataToClient. Which will just emit the content, but this way we are sure that content is done receiving the data from the file.
         sendFileDataToClient(content);
-
       }
     });
   }
+  
 };
