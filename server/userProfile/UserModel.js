aa5afe0f01042e4ba6aa9f34c3f18cc46f5c4d4f
BKhurshid <josephsun4@gmail.com>
2015-12-28 16:38:43 -0800

    deleted comments and old code.

--- server/userProfile/UserModel.js
+++ server/userProfile/UserModel.js
@@ -14,45 +14,11 @@ var userSchema = new Schema({
  messages:[{type: mongoose.Schema.Types.ObjectId, ref: 'Messages'}]
 });
 
- //this method would probably be best suited for our messages where skills would be messages
- //and the 
-
-// var skillsSchema = new mongoose.Schema({
-//   node: Number,
-//   angular: Number,
-//   html: Number,
-//   css: Number, 
-//   jquery: Number,
-//   userid: { type: Number, ref: 'User' }
-// });
-
-// var messageSchema = new Schema({
-//     created: String,
-//     text: String,
-//     displayName: String,
-//     room: String,
-//     userid: { type: Number, ref: 'User' }
-// });
-
-// var documentSchema = new Schema ({
-//     id: Number, 
-//     title: String, 
-//     mode: String,
-//     displayName: String, 
-//     code: String
-// });
-
-// var Document = mongoose.model('Document', documentSchema);
-// var Message = mongoose.model('Messages', messageSchema);
-// var Skills = mongoose.model("Skills", skillsSchema);
 var User = mongoose.model("User", userSchema);
 
 
 module.exports = {
   user: User
-  // messages: Message
-    // userDocument: Document,
-    // skills: Skills
 };
 
 
