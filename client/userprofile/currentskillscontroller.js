7fee73f3f7b4b288d7f2a7ea754e240e9f2cf44e
DhiMalo <aheri@Macbook-Pro.local>
2016-01-07 05:53:11 -0800

    fix: updated controller code to attempt checkbox fix

--- client/userprofile/currentskillscontroller.js
+++ client/userprofile/currentskillscontroller.js
@@ -1,12 +1,18 @@
 angular.module('myApp')
   .controller('CurrentSkillsController', ['$scope','profiledata', 'Account', function($scope, profiledata, Account){
+    
     $scope.currentskills = profiledata.skills;
     $scope.profile;
 
-    var account = Account.getUserDisplayName();
-    profiledata.findUser({user:account}).then(function(results){
-      $scope.profile = results.data.github;
-    })
+    // var account = Account.getUserDisplayName();
+    // profiledata.findUser({user:account}).then(function(results){
+    //   $scope.profile = results.data.github;
+    // })
+
+    Account.getProfile().success(function(data){
+    $scope.profile = data.profile.github
+    console.log('this is the data.profile:', data.profile.github)
+  })
 
     $scope.add = function(){
       profiledata.addSkills({github: $scope.profile, node:$scope.node, angular:$scope.angular, 
