00a3bddfff64a0e7207a27cb2342733bda149993
Carine TOdmia <ctodmia@gmail.com>
2016-02-29 11:43:15 -0800

    overflow hidden on body, background img, cover, no repeat on start temple

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/users', 
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'Shhh',
   GITHUB_SECRET: process.env.GITHUB_SECRET || '881163697cad6c7cc246638d4b98819dd5cf679e',
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '6ffd349ee17a258a13ff'
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '3c30b4028da7c634cb9a'
 };
