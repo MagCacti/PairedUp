0567e40ce5cf360ddb9bc309d2bf63fbcff0c1e7
Carine TOdmia <ctodmia@gmail.com>
2016-02-29 11:34:19 -0800

    general styling

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // App Settings
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://heroku_z1qhwknn:neo6ds37tnmv9q2256rosusqqe@ds061954.mongolab.com:61954/heroku_z1qhwknn', 
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/users', 
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'Shhh',
   GITHUB_SECRET: process.env.GITHUB_SECRET || '881163697cad6c7cc246638d4b98819dd5cf679e',
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '3c30b4028da7c634cb9a'
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '6ffd349ee17a258a13ff'
 };
